8f53b7859f384536a8601321937b73b22863a96f
Dom Scordino <dscordino001@student.franciscan.edu>
2022-12-03 19:29:22 -0500

    Add files via upload

---
+++ A_Scratch_File.py
@@ -0,0 +1,21 @@
+# Make function Prime_q
+	# Takes an inputted integer and tests whether its prime
+	# If number = prime  true
+	# If number not prime  false
+    # if number <= 1  return None
+# make function 1_to_100
+    # find and list all prime numbers 1-100
+# make test_prime function
+    # tests two numbers
+        # one prime and one composite
+    # display number and say whether its prime or not
+def prime_q():
+
+def one_to_100():
+
+def test_prime():
+
+def main():
+
+if __name__ == '__main__':
+    main()
